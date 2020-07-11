984b0371f66c8cf9522c3a3ca7540be51b786eed
Otávio Carvalho <otavioarc@outlook.com>
2020-07-11 10:58:14 -0300

    Remove demo application (#1)

--- src/main/java/com/hermes/demo/domain/commands/GetDateCommand.java
+++
@@ -1,8 +0,0 @@
-package com.hermes.demo.domain.commands;
-
-import com.hermes.core.requests.IRequest;
-
-import java.util.Date;
-
-public class GetDateCommand implements IRequest<Date> {
-}
--- src/main/java/com/hermes/demo/domain/commands/GreetingCommand.java
+++
@@ -1,6 +0,0 @@
-package com.hermes.demo.domain.commands;
-
-import com.hermes.core.requests.IRequest;
-
-public class GreetingCommand implements IRequest<String> {
-}
