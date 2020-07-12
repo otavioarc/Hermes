f0ce28f65951c4d2df332114612528fd7df358c7
Otávio Carvalho <otavioarc@outlook.com>
2020-07-12 00:21:41 -0300

    Add Java Doc (#5)

--- src/main/java/com/hermes/IMessage.java
+++ src/main/java/com/hermes/IMessage.java
@@ -1,4 +1,10 @@
 package com.hermes;
 
+/**
+ * Interface used to implements messages that should be treated by a message handler.
+ * @author Otavio Carvalho
+ * @since 0.0.1
+ * @param <ResponseT> the class type that handler should return.
+ */
 public interface IMessage<ResponseT extends Object> {
 }
